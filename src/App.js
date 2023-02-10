9be07f20e4e293fbfcab3f77b340f1662f3df8c1
shweta <shwetaa3007@gmail.com>
2023-02-09 12:38:17 -1200

    change

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import InputForm from "./Components/InputForm/LoginForm";
 import Astro from "./Components/Astro/Zodic";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.js";
 
